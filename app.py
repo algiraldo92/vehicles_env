5a94bb3cb0196d9e733c06057330992899548767
Alan Giraldo <alangiraldo18@gmail.com>
2025-06-01 16:03:52 -0500

    se agregan las casillas de verificaciones

--- app.py
+++ app.py
@@ -21,3 +21,17 @@ if scat_button:
     st.write('Creacion de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
     fig = px.scatter(car_data, y = 'odometer')
     st.plotly_chart(fig,use_container_width=True)
+
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    fig = px.histogram(car_data, x ='odometer')
+    st.plotly_chart(fig,use_container_width=True)
+
+build_scatter = st.checkbox('Construir un grafico de dispersion')
+
+if build_scatter: # si la casilla de verificación está seleccionada
+    st.write('Construir un grafico de dispersion')
+    fig = px.scatter(car_data, y = 'odometer')
+    st.plotly_chart(fig,use_container_width=True)
